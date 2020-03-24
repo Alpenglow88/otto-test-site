1e0176d68154756bd81821c1eddc4a64ec0dfc10
Alpenglow88 <ian.goddard@theculturetrip.com>
2020-03-24 21:19:38 +0000

    adding otto tool info and editing footer

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const package = require('./package.json');
-
 const version = package.version
 
 // Exposes css from public folder to Express
