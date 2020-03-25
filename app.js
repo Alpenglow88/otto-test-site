7d9a380db779d77428f1631bb9b0255d6756a5c6
Alpenglow88 <ian.goddard@theculturetrip.com>
2020-03-25 16:04:49 +0000

    adding new test page content

--- app.js
+++ app.js
@@ -1,72 +1,81 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const package = require('./package.json');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const package = require('./package.json')
 const version = package.version
 
 // Exposes css from public folder to Express
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Sets view enngine as EJS
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Sets root routing and defines what is seen there
 app.get('/', (req, res) => {
-  res.render('pages/landing-page', {version: version});
-});
+  res.render('pages/landing-page', { version: version })
+})
 
 app.get('/index', (req, res) => {
-  res.render('pages/index', {version: version});
-});
-
-// app.post('/', (req, res) => {
-//     res.render('initial-test-page')
-// })
+  res.render('pages/index', { version: version })
+})
 
 app.get('/test-pages-home', (req, res) => {
-  res.render('pages/test_pages/test-pages-home', {version: version});
-});
+  res.render('pages/test_pages/test-pages-home', { version: version })
+})
 
 app.get('/about', (req, res) => {
-  res.render('pages/about-page', {version: version});
-});
+  res.render('pages/about-page', { version: version })
+})
 
 app.get('/waiting', (req, res) => {
-  res.render('pages/waiting', {version: version});
-});
+  res.render('pages/waiting', { version: version })
+})
 
 app.get('/test', (req, res) => {
-  res.render('pages/waiting', {version: version});
-});
+  res.render('pages/waiting', { version: version })
+})
 
 app.get('/otto-tool', (req, res) => {
-  res.render('pages/otto-tool-page', {version: version});
-});
+  res.render('pages/otto-tool-page', { version: version })
+})
 
 app.get('/lessons', (req, res) => {
-  res.render('pages/lessons-page', {version: version});
-});
+  res.render('pages/lessons-page', { version: version })
+})
 
+app.get('/return', (req, res) => {
+  res.render('pages/return', { version: version })
+})
 
-// app.post('/index', function (req, res) {
+app.get('/success-button-click', (req, res) => {
+  res.render('pages/success-button-click', { version: version })
+})
+
+app.post('/success-text-entry', (req, res) => {
+  let text = req.body.text_entry
+    res.render('pages/test_pages/test-page-text-entry-success', {version: version, text: text})
+})
+
+// Sets application to be on port 3000 or dynamic port assigned from Heroku
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`The app is running on port ${PORT}`)
+})
+
+// app.post('/test-pages-home', function (req, res) {
 //     let city = req.body.city;
 
 //     if(city === 'Cape Town'){
 //         let passingReturnText = `Bravo, ${city} is correct`
-//         res.render('return', {city: city, returnText: passingReturnText});
+//         res.render('pages/return', {city: city, returnText: passingReturnText});
 //     } else {
 //         let failingReturnText = `${city} is not Cape Town :(`
-//         res.render('return', {city: city, returnText: failingReturnText});
+//         res.render('pages/return', {city: city, returnText: failingReturnText});
 //         }
 // })
 
-// Sets application to be on port 3000 or dynamic port assigned from heroku
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Our app is running on port ${ PORT }`);
-});
+// app.post('/test-pages-home', (req, res) => {
+//     res.render('pages/return')
+// })
