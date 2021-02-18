551b45991cbd7251f74212dc14e5ce0dc6eb26df
Alpenglow88 <igoddard77@gmail.com>
2021-02-18 15:40:36 +0000

    Release commit for Version 1.2.3

--- app.js
+++ app.js
@@ -101,6 +101,12 @@ app.get("/contact", (req, res) => {
   });
 });
 
+app.get("/duplicate-id", (req, res) => {
+  res.render("pages/test_pages/duplicate-test-id", {
+    version: version,
+  });
+});
+
 // Sets application to be on port 3000 or dynamic port assigned from Heroku
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
